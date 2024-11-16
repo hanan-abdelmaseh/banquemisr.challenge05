2ab015de048c7808f49540575f94b5d86740ed97
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-11-16 16:02:47 +0200

    making service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from  '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -16,7 +17,8 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/converter/services/converter.service.ts
+++ src/app/converter/services/converter.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Converter, HistoricalRates, SymbolsResponse } from '../Models/converter.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConverterService {
 
-  constructor() { }
+
+  apiUrl:string ="https://data.fixer.io/api"
+
+  apiKey:string = '8105562f5fd150443029c7560772f2fd';  
+
+ //latest?access_key=8105562f5fd150443029c7560772f2fd;
+
+ // /latest ? access_key = a1fa04f892fe4e77a5b3ade0c2b31f3d"
+ //https://data.fixer.io/api/2023-12-24?base=EUR&symbols
+
+ constructor(private _HttpClient:HttpClient) { }
+
+
+
+ getCurrencyLatestValues():Observable<Converter>{
+    return this._HttpClient.get<Converter>(`${this.apiUrl}/latest?access_key=${this.apiKey}`)
+ }
+ getLatestRateRateForSingleSymbol(base:string, toCurrency:string):Observable<any>{
+ return this._HttpClient.get<any>(`${this.apiUrl}/latest?base=${base}&symbols=${toCurrency}`)
+ 
+ }
+ getLatestRateForAllSymbols(base:string):Observable<any>{
+   return this._HttpClient.get<any>(`${this.apiUrl}/latest?base=${base}&symbols`)
+ }
+
+ getHistoricalRatesFromToAllSymbols(date:string, base:string):Observable<HistoricalRates>{
+  return this._HttpClient.get<HistoricalRates>(`${this.apiUrl}/${date}?base=${base}&symbols&access_key=${this.apiKey}`)
+ }
+
+ getHistoricalRatesFromToOneSymbol(date:string, base:string,symbol:string):Observable<HistoricalRates>{
+   return this._HttpClient.get<HistoricalRates>(`${this.apiUrl}/${date}?base=${base}&symbols=${symbol}&access_key=${this.apiKey}`)
+  
+ 
+ }
+
+ getCureenciesNames():Observable<SymbolsResponse>{
+   return this._HttpClient.get<SymbolsResponse>(`${this.apiUrl}/symbols?access_key=${this.apiKey}`)
+ }
+
+ //convert
+ convertCurrency(fromCurrency:string, toCurrency:string, amount:number):Observable<any>{
+   return this._HttpClient.get<SymbolsResponse>(`${this.apiUrl}/convert?access_key=${this.apiKey}&from=${fromCurrency}&to=${toCurrency}&amount=${amount}`)
+ } 
+  
+ 
 }
