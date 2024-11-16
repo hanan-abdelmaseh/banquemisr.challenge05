e8a30ce6d28e4d1d3363017602386203a3cf3f6f
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-11-16 22:02:42 +0200

    updating currency detail

--- src/app/converter/components/currency-details/currency-details.component.ts
+++ src/app/converter/components/currency-details/currency-details.component.ts
@@ -11,6 +11,8 @@ export class CurrencyDetailsComponent implements OnInit {
   fromCurrency:string ='';
   toCurrency:string=''
   ratesList!:any;
+  recentRatesList!:any
+  popularRatesList:any;
   constructor(private _ActivatedRoute: ActivatedRoute,
     private _Router: Router,  private _ConverterService :ConverterService){
 
@@ -21,10 +23,17 @@ export class CurrencyDetailsComponent implements OnInit {
         this.fromCurrency = params['from'];
         this.toCurrency= params['to']
      });
-     this.getHistoricalRates()
+     this.getHistoricalRates();
+     this.getLatestRateRateForSingleSymbol();
+     this.getLatestRateForMultibleCurrenciesForBaseSymbol();
   }
 
-
+  getLatestRateRateForSingleSymbol(){
+    this._ConverterService.getLatestRateRateForSingleSymbol(this.fromCurrency,this.toCurrency).subscribe((res)=>{
+      console.log(res);
+      this.recentRatesList= res.rates
+    })
+  }
   getHistoricalRates(){
     const today = new Date();
     this._ConverterService.getHistoricalRatesFromToOneSymbol(`${today.getFullYear() - 1}-11-15`, this.fromCurrency,this.toCurrency).subscribe((res)=>{
@@ -34,6 +43,13 @@ export class CurrencyDetailsComponent implements OnInit {
    
 
     //console.log( Object.keys(this.ratesList).length)
+    })
+  }
+
+  getLatestRateForMultibleCurrenciesForBaseSymbol(){
+    this._ConverterService.getLatestRateForMultibleCurrenciesForBaseSymbol(this.fromCurrency).subscribe((res)=>{
+      console.log(res);
+      this.popularRatesList = res.rates
     })
   }
 }
--- src/app/converter/services/converter.service.ts
+++ src/app/converter/services/converter.service.ts
@@ -11,9 +11,11 @@ export class ConverterService {
 
   apiUrl:string ="https://data.fixer.io/api"
 
-  apiKey:string = '8105562f5fd150443029c7560772f2fd';  
+  apiKey:string = 'f13d09103872912c75772500e4668785';  
 
  //latest?access_key=8105562f5fd150443029c7560772f2fd;
+ //f13d09103872912c75772500e4668785
+ //8105562f5fd150443029c7560772f2fd
 
  // /latest ? access_key = a1fa04f892fe4e77a5b3ade0c2b31f3d"
  //https://data.fixer.io/api/2023-12-24?base=EUR&symbols
@@ -25,8 +27,9 @@ export class ConverterService {
  getCurrencyLatestValues():Observable<Converter>{
     return this._HttpClient.get<Converter>(`${this.apiUrl}/latest?access_key=${this.apiKey}`)
  }
+
  getLatestRateRateForSingleSymbol(base:string, toCurrency:string):Observable<any>{
- return this._HttpClient.get<any>(`${this.apiUrl}/latest?base=${base}&symbols=${toCurrency}`)
+ return this._HttpClient.get<any>(`${this.apiUrl}/latest?access_key=${this.apiKey}&base=${base}&symbols=${toCurrency}`)
  
  }
  getLatestRateForAllSymbols(base:string):Observable<any>{
@@ -52,5 +55,10 @@ export class ConverterService {
    return this._HttpClient.get<SymbolsResponse>(`${this.apiUrl}/convert?access_key=${this.apiKey}&from=${fromCurrency}&to=${toCurrency}&amount=${amount}`)
  } 
   
+ getLatestRateForMultibleCurrenciesForBaseSymbol(base:string):Observable<any>{
+  return this._HttpClient.get(`${this.apiUrl}/latest?access_key=${this.apiKey}&base=${base}&symbols=USD,CAD,JPY,GHS,GNF`)
+
+
+ }
  
 }
