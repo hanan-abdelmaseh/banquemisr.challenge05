aa883eddc4c691e99e3b1b255ea39a5db4dd2ba2
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-11-16 16:17:01 +0200

    implement converter module components

--- src/app/converter/components/eur-details/eur-details.component.ts
+++ src/app/converter/components/eur-details/eur-details.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Symbols } from '../../Models/converter.model';
+import { ConverterService } from '../../services/converter.service';
 
 @Component({
   selector: 'app-eur-details',
   templateUrl: './eur-details.component.html',
   styleUrls: ['./eur-details.component.scss']
 })
-export class EurDetailsComponent {
+export class EurDetailsComponent  implements OnInit{
+  fromCurrency:string ='';
+  currencyTo:string =''
+  currencyNamesList!:Symbols;
+  currencyForm!:FormGroup;
+  constructor(private _ActivatedRoute: ActivatedRoute,private _ConverterService:ConverterService,
+    private _Router: Router,private _FB:FormBuilder){
 
+  }
+  
+
+  ngOnInit(): void {
+     this._ActivatedRoute.queryParams.subscribe((params)=>{
+        this.fromCurrency = params['from'];
+
+     });
+     this.currencyForm = this._FB.group({
+      amount: ['' ,Validators.required],
+      currencyForm: [this.fromCurrency, Validators.required],
+      currencyTo: ['', Validators.required]
+    });
+
+     this.getCurrencyValues();
+  }
+
+
+  
+  getCurrencyValues(){
+    this._ConverterService.getCureenciesNames().subscribe((res)=>{
+      console.log(res);
+      this.currencyNamesList= res.symbols;
+     
+    })
+  }
+
+
+
+
+  convertCurrency(currencyValues:any){
+    let data = currencyValues.value;
+   this.currencyTo = data.currencyTo;
+    console.log(this.currencyTo);
+//using api  
+// this._ConverterService.convertCurrency(data.fromCurrency, data.toCurrency,data.amount).subscribe((res)=>{
+//   console.log(res)
+// })
+}
 }
--- src/app/converter/components/historical-rates/historical-rates.component.ts
+++ src/app/converter/components/historical-rates/historical-rates.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Rates } from '../../Models/converter.model';
+import { ConverterService } from '../../services/converter.service';
 
 @Component({
   selector: 'app-historical-rates',
   templateUrl: './historical-rates.component.html',
   styleUrls: ['./historical-rates.component.scss']
 })
-export class HistoricalRatesComponent {
+export class HistoricalRatesComponent implements OnInit {
+  ratesList!:Rates;
+  basicFrom:string='';
 
+  fromCurrency :string =''
+  
+
+  constructor(private _ConverterService:ConverterService , private _ActivatedRoute:ActivatedRoute){
+    // this.basicFrom =this._ActivatedRoute.snapshot.queryParams['from'];
+    // console.log(this.basicFrom)
+  }
+  ngOnInit(): void {
+    this._ActivatedRoute.queryParams.subscribe((params)=>{
+      this.fromCurrency = params['from'];
+
+   });
+
+   this.getHistoricalRates();
+  }
+  
+  getHistoricalRates(){
+    const today = new Date();
+    this._ConverterService.getHistoricalRatesFromToAllSymbols(`${today.getFullYear() - 1}-11-15`, this.fromCurrency).subscribe((res)=>{
+    this.ratesList = res.rates;
+    // this.fromCurrency = res.base;
+    console.log(this.ratesList)
+   
+
+    //console.log( Object.keys(this.ratesList).length)
+    })
+  }
+ 
 }
